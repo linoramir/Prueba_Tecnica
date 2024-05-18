8785729a1914d658fd42f648092417b07af5dc79
linoramir <ramirezlinogabriel@gmail.com>
2024-05-18 20:29:53 -0300

    defined dimensions of the elements

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,6 +1,8 @@
 const SideBar = () => {
   return (
-    <div className="w-1/4 border-solid border-2 border-sky-500">SideBar</div>
+    <div className="w-1/4 border-solid border-2 border-sky-500 H-Full bg-moralis-sidebar">
+      SideBar
+    </div>
   );
 };
 export default SideBar;
