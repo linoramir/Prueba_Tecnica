cd30b1d265ea9478e576ec311c01552ab76e15ea
linoramir <ramirezlinogabriel@gmail.com>
2024-05-19 09:59:19 -0300

    progress in paging

--- src/components/paged/index.tsx
+++ src/components/paged/index.tsx
@@ -1,7 +1,44 @@
 const Paged = () => {
   return (
-    <div className="border-solid border-2 border-sky-500 bg-moralis-bubble-container h-11">
-      Paginado
+    <div className="border-solid border-2 border-sky-500 bg-moralis-bubble-container h-11 px-8 py-1">
+      <div>
+        <button className="join-item btn btn-outline border-solid border-2 rounded border-slate-700 text-slate-200 w-8">
+          {"<-"}
+        </button>
+        <button className="border-solid border-2 rounded border-slate-700 text-slate-200 w-8 mx-1">
+          1
+        </button>
+        <button className="border-solid border-2 rounded border-slate-700 text-slate-200 w-8 mx-1">
+          2
+        </button>
+        <button className="border-solid border-2 rounded border-slate-700 text-slate-200 w-8 mx-1">
+          3
+        </button>
+        <button className="border-solid border-2 rounded border-slate-700 text-slate-200 w-8 mx-1">
+          4
+        </button>
+        <button className="border-solid border-2 rounded border-slate-700 text-slate-200 w-8 mx-1">
+          5
+        </button>
+        <button className="border-solid border-2 rounded border-slate-700 text-slate-200 w-8 mx-1">
+          6
+        </button>
+        <button className="border-solid border-2 rounded border-slate-700 text-slate-200 w-8 mx-1">
+          7
+        </button>
+        <button className="border-solid border-2 rounded border-slate-700 text-slate-200 w-8 mx-1">
+          8
+        </button>
+        <button className="border-solid border-2 rounded border-slate-700 text-slate-200 w-8 mx-1">
+          9
+        </button>
+        <button className="border-solid border-2 rounded border-slate-700 text-slate-200 w-8 mx-1">
+          10
+        </button>
+        <button className="join-item btn btn-outline border-solid border-2 rounded border-slate-700 text-slate-200 w-8 mx-1">
+          {"->"}
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,31 +1,31 @@
 const SideBar = () => {
   return (
     <div className="w-1/4 border-solid border-2 border-sky-500 H-Full bg-moralis-sidebar px-8">
-      <button className="w-4/5 border-solid border-2 border-transparent text-wrap text-slate-200 px-8 py-8">
+      <button className="w-full border-solid border-2 border-transparent text-nowrap text-slate-200 px-8 py-8">
         Jaja Moralis pirata
       </button>
       <nav>
-        <button className="w-4/5 border-solid border-2 border-transparent transition ease-in-out delay-50 hover:bg-sky-900 duration-150 rounded-lg text-slate-300 px-2 py-2">
+        <button className="w-60 border-solid border-2 border-transparent transition ease-in-out delay-50 hover:bg-sky-900 duration-150 rounded-lg text-slate-300 px-2 py-2">
           Market Overview ↓
         </button>
-        <button className="w-4/5 border-solid border-2 border-transparent transition ease-in-out delay-50 hover:bg-sky-900 duration-150 rounded-lg text-slate-300 px-2 py-2">
+        <button className="w-60 border-solid border-2 border-transparent transition ease-in-out delay-50 hover:bg-sky-900 duration-150 rounded-lg text-slate-300 px-2 py-2">
           Research ↓
         </button>
-        <button className="w-4/5 border-solid border-2 border-transparent transition ease-in-out delay-50 hover:bg-sky-900 duration-150 rounded-lg text-slate-300 py-2 px-2">
+        <button className="w-60 border-solid border-2 border-transparent transition ease-in-out delay-50 hover:bg-sky-900 duration-150 rounded-lg text-slate-300 py-2 px-2">
           Saved
         </button>
-        <button className="w-4/5 border-solid border-2 border-transparent transition ease-in-out delay-50 hover:bg-sky-900 duration-150 rounded-lg text-slate-300 py-2 px-2">
+        <button className="w-60 border-solid border-2 border-transparent transition ease-in-out delay-50 hover:bg-sky-900 duration-150 rounded-lg text-slate-300 py-2 px-2">
           Learn ↓
         </button>
-        <button className="w-4/5 border-solid border-2 border-transparent transition ease-in-out delay-50 hover:bg-sky-900 duration-150 rounded-lg text-slate-300 py-2 px-2">
+        <button className="w-60 border-solid border-2 border-transparent transition ease-in-out delay-50 hover:bg-sky-900 duration-150 rounded-lg text-slate-300 py-2 px-2">
           Seattings
         </button>
-        <button className="w-4/5 border-solid border-2 border-transparent transition ease-in-out delay-50 hover:bg-sky-900 duration-150 rounded-lg text-slate-300 py-2 px-2">
+        <button className="w-60 border-solid border-2 border-transparent transition ease-in-out delay-50 hover:bg-sky-900 duration-150 rounded-lg text-slate-300 py-2 px-2">
           Api for Devs
         </button>
       </nav>
-      <button className=" w-5/6 border-solid border-2 border-sky-900 transition ease-in-out delay-50 hover:border-slate-400 hover:text-slate-200 duration-150 rounded-lg text-slate-400 px-2 py-2">
-        {"-> Start Trial"}
+      <button className="w-full border-solid border-2 border-sky-900 transition ease-in-out delay-50 hover:border-slate-400 hover:text-slate-200 duration-150 rounded-lg text-slate-400 px-2 py-2">
+        {"→ Start Trial"}
       </button>
     </div>
   );
