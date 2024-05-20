abf1fe7e003f315185576dc85ab0cb055a62ad9d
linoramir <ramirezlinogabriel@gmail.com>
2024-05-19 21:11:45 -0300

    add bubble

--- src/components/bubbleContainer/index.tsx
+++ src/components/bubbleContainer/index.tsx
@@ -1,10 +1,20 @@
-// import { getCryptoList } from "../../service/coinGeckoApi";
+import getCryptoList from "../../service/coinGeckoApi";
+import { useState, useEffect } from "react";
 import Bubble from "../bubble";
+
 const BubbleContainer = () => {
+  const [criptoList, setCriptoList] = useState([]);
+  useEffect(() => {
+    const criptoData = async () => {
+      const data = await getCryptoList();
+      console.log("bubble container", { data });
+      setCriptoList(data);
+    };
+    criptoData();
+  }, []);
   return (
     <div className="w-full h-4/5 bg-moralis-bubble-container">
-      BubbleContainer
-      <Bubble />
+      <Bubble criptoList={criptoList} />
     </div>
   );
 };
